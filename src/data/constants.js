396c126dc90dc0a0d442eb654ad2df3786f4c01b
Azad Gupta <physicswallahlakshyabatch@gmail.com>
2024-10-30 11:34:08 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -401,33 +401,24 @@ export const experiences = [
 export const education = [
   {
     id: 0,
-    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/Kiit.jpeg?alt=media&token=3839d520-c59d-4341-ad8f-0980c98de4dd",
-    school: "Kalinga Institute of Industrial Technology, Bhubaneswar",
-    date: "Oct 2021 - Sep 2025",
-    grade: "8.41 CGPA",
-    desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at Kalinga Institute of Industrial Technology, Bhubaneswar. I have completed 4 semesters and have a CGPA of 8.71. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. I am also a member of the Google Developers Student Club (GDSC) at KIIT, where I am learning and working on exciting projects with a team of talented developers.",
-    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+    img: "https://examyou.com/assets/uploads/examboards/institute-informatics-communication.png?alt=media&token=3839d520-c59d-4341-ad8f-0980c98de4dd",
+    school: "Institute of Informatics and Communication, Delhi University",
+    date: "Oct 2023 - July 2025",
+    grade: "7.4 CGPA",
+    desc: "I am currently pursuing my Master’s degree at the Institute of Informatics and Communication, Delhi University, and have completed two semesters. My coursework includes Data Structures, Algorithms, Object-Oriented Programming, and Database Management Systems.",
+    degree: "Masters of Science - M.Sc., Informatics",
   },
   {
     id: 1,
-    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/methodist.png?alt=media&token=018a1b18-e2dd-4f34-8855-cff1b6b69fd3",
-    school: "Methodist School, Dankuni",
-    date: "Apr 2019 - Apr 2025",
-    grade: "88.2%",
-    desc: "I completed my class 12 high school education at Methodist School, Dankuni, where I studied Science with Computer Science.",
-    degree: "ISC(XII), Science with Computer",
-  },
-  {
-    id: 2,
-    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/methodist.png?alt=media&token=018a1b18-e2dd-4f34-8855-cff1b6b69fd3",
-    school: "Methodist School, Dankuni",
-    date: "Apr 2017 - Apr 2019",
-    grade: "92.3%",
-    desc: "I completed my class 10 education at Methodist School, Dankuni, where I studied Science with Computer Application.",
-    degree: "ICSC(X), Science with Computer",
+    img: "https://www.andcollege.du.ac.in/assets/front/images/logo.png?alt=media&token=018a1b18-e2dd-4f34-8855-cff1b6b69fd3",
+    school: "ANDC, Delhi University",
+    date: "Sep 2020 - Apr 2023",
+    grade: "7.4 CGPA",
+    desc: "I completed my Bachelor's degree in BSc Electronic Science from Acharya Narendra Dev College (ANDC), Delhi University, earning a CGPA of 7.4. This program provided me with a solid foundation in electronics and related technologies.",
+    degree: "B.Sc, Electronic SCience",
   },
 ];
-
+/*
 export const projects = [
   {
     id: 11,
@@ -641,3 +632,4 @@ export const projects = [
   },
 ];
 
+*/
